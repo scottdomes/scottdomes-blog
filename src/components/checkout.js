280368d0279de954f804968e912ac135d1aec2d3
scottdomes <scott@musefind.com>
2020-04-18 11:38:04 -0700

    Renamed vars

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -2,12 +2,12 @@ import React from "react"
 import { loadStripe } from "@stripe/stripe-js"
 import styles from './styles/checkout.module.css'
 
-const stripePromise = loadStripe(process.env.STRIPE_KEY)
+const stripePromise = loadStripe(process.env.GATSBY_STRIPE_KEY)
 const redirectToCheckout = async event => {
   event.preventDefault()
   const stripe = await stripePromise
   const { error } = await stripe.redirectToCheckout({
-    items: [{ sku: process.env.STRIPE_PRESALE_SKU, quantity: 1 }],
+    items: [{ sku: process.env.GATSBY_STRIPE_PRESALE_SKU, quantity: 1 }],
     successUrl: `${window.location.origin}/thanks/`,
     cancelUrl: `${window.location.origin}/react-developer-challenge/`,
   })
