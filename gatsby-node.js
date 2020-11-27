55ee3d080fb97520935398c1f88867c05a9f53f0
scottdomes <scottdomes@gmail.com>
2020-11-27 10:30:07 -0800

    Basic sales template

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,15 @@
-const path = require(`path`);
-const { createFilePath } = require(`gatsby-source-filesystem`);
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
 
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions;
-  const blogPost = path.resolve(`./src/templates/blog-post.js`);
+  const { createPage } = actions
+  const blogPost = path.resolve(`./src/templates/blog-post.js`)
+  const salesTemplate = path.resolve(`./src/templates/sales.js`)
+
   return graphql(
     `
       {
@@ -22,49 +24,61 @@ exports.createPages = ({ graphql, actions }) => {
               }
               frontmatter {
                 title
+                sales
               }
             }
           }
         }
       }
-    `,
-  ).then((result) => {
+    `
+  ).then(result => {
     if (result.errors) {
-      throw result.errors;
+      throw result.errors
     }
 
     // Create blog posts pages.
-    const posts = result.data.allMdx.edges;
+    const posts = result.data.allMdx.edges
 
     posts.forEach((post, index) => {
-      const previous =
-        index === posts.length - 1 ? null : posts[index + 1].node;
-      const next = index === 0 ? null : posts[index - 1].node;
+      const previous = index === posts.length - 1 ? null : posts[index + 1].node
+      const next = index === 0 ? null : posts[index - 1].node
 
-      createPage({
-        path: post.node.fields.slug,
-        component: blogPost,
-        context: {
-          slug: post.node.fields.slug,
-          previous,
-          next,
-        },
-      });
-    });
+      if (post.node.frontmatter.sales) {
+        createPage({
+          path: post.node.fields.slug,
+          component: salesTemplate,
+          context: {
+            slug: post.node.fields.slug,
+            previous,
+            next,
+          },
+        })
+      } else {
+        createPage({
+          path: post.node.fields.slug,
+          component: blogPost,
+          context: {
+            slug: post.node.fields.slug,
+            previous,
+            next,
+          },
+        })
+      }
+    })
 
-    return null;
-  });
-};
+    return null
+  })
+}
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions;
+  const { createNodeField } = actions
 
   if (node.internal.type === `Mdx`) {
-    const value = createFilePath({ node, getNode });
+    const value = createFilePath({ node, getNode })
     createNodeField({
       name: `slug`,
       node,
       value,
-    });
+    })
   }
-};
+}
---
+++ src/templates/sales.js
@@ -0,0 +1,58 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { rhythm, scale } from "../utils/typography"
+import { MDXRenderer } from "gatsby-plugin-mdx"
+
+class SalesTemplate extends React.Component {
+  render() {
+    const post = this.props.data.mdx
+    const siteTitle = this.props.data.site.siteMetadata.title
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO
+          title={post.frontmatter.title}
+          description={post.frontmatter.description || post.excerpt}
+        />
+        <div
+          style={{
+            marginBottom: rhythm(1),
+          }}
+        >
+          <h1>{post.frontmatter.title}</h1>
+        </div>
+        <MDXRenderer>{post.body}</MDXRenderer>
+        <hr
+          style={{
+            marginBottom: rhythm(1),
+          }}
+        />
+      </Layout>
+    )
+  }
+}
+
+export default SalesTemplate
+
+export const pageQuery = graphql`
+  query SalesInfoBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+        author
+      }
+    }
+    mdx(fields: { slug: { eq: $slug } }) {
+      id
+      excerpt(pruneLength: 160)
+      body
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+        description
+      }
+    }
+  }
+`
