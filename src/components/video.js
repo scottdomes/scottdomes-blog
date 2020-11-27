acfa24e59eff9c84998e500c1c2238dfd8a57d5c
scottdomes <scottdomes@gmail.com>
2020-11-27 11:51:24 -0800

    playsInline

--- src/components/video.js
+++ src/components/video.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Video = ({ src, style }) => {
   return (
-    <video autoPlay muted loop style={style}>
+    <video autoPlay muted loop style={style} playsInline>
       <source src={src} type="video/mp4" />
     </video>
   )
