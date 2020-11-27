1e2eed88913aaac3880732dd8c036c82ee680923
scottdomes <scottdomes@gmail.com>
2020-11-27 12:04:37 -0800

    Giffing it

--- src/components/videos/HooksVideo.js
+++ src/components/videos/HooksVideo.js
@@ -1,8 +1,15 @@
 import React from "react"
 import Video from "../video"
 import Hooks from "./hooks-cover.mp4"
+import Gif from "./hooks.gif"
 
 const HooksVideo = ({}) => {
-  return <Video style={{ width: "100%" }} src={Hooks} />
+  // ;<Video style={{ width: "100%" }} src={Hooks} />
+
+  return (
+    <>
+      <img src={Gif} />
+    </>
+  )
 }
 export default HooksVideo
