1f16e83b64af98441d243ecf03be284d6080e53f
scottdomes <scottdomes@gmail.com>
2021-03-06 14:16:27 -0800

    labels

--- src/components/WolframPattern.js
+++ src/components/WolframPattern.js
@@ -1,21 +1,24 @@
 import React, { useState } from "react"
 import styles from "./styles/WolframPattern.module.css"
 
-const RULE_90 = (cell, leftNeighbor, rightNeighbor) => {
-  return (
-    (leftNeighbor === 1 || rightNeighbor === 1) &&
-    leftNeighbor !== rightNeighbor
-  )
+const RULES = {
+  30: (previousCell, leftNeighbor, rightNeighbor) => {
+    return (
+      (leftNeighbor && !(previousCell || rightNeighbor)) ||
+      (!leftNeighbor && (previousCell || rightNeighbor))
+    )
+  },
+  90: (cell, leftNeighbor, rightNeighbor) => {
+    return (
+      (leftNeighbor === 1 || rightNeighbor === 1) &&
+      leftNeighbor !== rightNeighbor
+    )
+  },
+  254: (cell, leftNeighbor, rightNeighbor) => {
+    return leftNeighbor === 1 || rightNeighbor === 1 || cell === 1
+  },
 }
 
-const RULE_30 = (previousCell, leftNeighbor, rightNeighbor) => {
-  return (
-    (leftNeighbor && !(previousCell || rightNeighbor)) ||
-    (!leftNeighbor && (previousCell || rightNeighbor))
-  )
-}
-
-const NUMBER_OF_ROWS = 50
 const getEmptyRow = () => {
   return Array(101)
     .fill()
@@ -44,11 +47,11 @@ const updateRow = (row, previousRow, rule) => {
   })
 }
 
-const WolframPattern = () => {
+const WolframPattern = ({ defaultRule }) => {
   initialRows[0] = firstRow
   const [rows, setRows] = useState(initialRows)
+  const [error, setError] = useState("")
   const [customRule, setCustomRule] = useState([])
-  // const rule = RULE_30
 
   const customRuleEval = (cell, leftNeighbor, rightNeighbor) => {
     return eval(customRule.join(" "))
@@ -57,7 +60,8 @@ const WolframPattern = () => {
   const getUpdatedRows = () => {
     const newRows = [...rows]
     for (let i = 1; i < rows.length; i++) {
-      newRows[i] = updateRow(newRows[i], newRows[i - 1], customRuleEval)
+      const rule = defaultRule ? RULES[defaultRule] : customRuleEval
+      newRows[i] = updateRow(newRows[i], newRows[i - 1], rule)
     }
     return newRows
   }
@@ -73,6 +77,10 @@ const WolframPattern = () => {
   const updateRows = () => {
     const isWorking = testCustomRule()
     console.log("Works?", isWorking)
+
+    if (!isWorking) {
+      return setError("Unable to process rule")
+    }
     const rowsToUpdate = getUpdatedRows()
 
     setRows(rowsToUpdate)
@@ -100,7 +108,7 @@ const WolframPattern = () => {
   console.log(customRule)
 
   return (
-    <div>
+    <div className={styles.container}>
       <div className={styles.rowContainer}>
         {rows.map((row, rowIndex) => {
           return (
@@ -119,26 +127,69 @@ const WolframPattern = () => {
           )
         })}
       </div>
-      <button onClick={updateRows}>Run</button>
-      <button onClick={resetRows}>Reset</button>
-      <p>{customRule.join(" ")}</p>
-      <div>
-        <button onClick={() => addToRule("(")}>(</button>
-        <button onClick={() => addToRule(")")}>)</button>
-        <button onClick={() => addToRule("&&")}>AND</button>
-        <button onClick={() => addToRule("||")}>OR</button>
-        <button onClick={() => addToRule("===")}>EQUALS</button>
-        <button onClick={() => addToRule("!==")}>DOES NOT EQUAL</button>
-        <button onClick={() => addToRule("!")}>NOT</button>
-        <button onClick={() => addToRule("leftNeighbor")}>Left neighbor</button>
-        <button onClick={() => addToRule("rightNeighbor")}>
-          Right neighbor
+      <div className={styles.buttonContainer}>
+        <button className={styles.button} onClick={updateRows}>
+          Run
         </button>
-        <button onClick={() => addToRule("cell")}>Cell</button>
-        <button onClick={() => addToRule("=== 1")}>IS FILLED</button>
-        <button onClick={deleteFromRule}>Delete</button>
-        <button onClick={clearCustomRule}>Clear</button>
+        <button className={styles.button} onClick={resetRows}>
+          Reset
+        </button>
       </div>
+      {!defaultRule && (
+        <div>
+          <p>{customRule.join(" ")}</p>
+          <div>
+            <button className={styles.button} onClick={() => addToRule("(")}>
+              (
+            </button>
+            <button className={styles.button} onClick={() => addToRule(")")}>
+              )
+            </button>
+            <button className={styles.button} onClick={() => addToRule("&&")}>
+              AND
+            </button>
+            <button className={styles.button} onClick={() => addToRule("||")}>
+              OR
+            </button>
+            <button className={styles.button} onClick={() => addToRule("===")}>
+              EQUALS
+            </button>
+            <button className={styles.button} onClick={() => addToRule("!==")}>
+              DOES NOT EQUAL
+            </button>
+            <button className={styles.button} onClick={() => addToRule("!")}>
+              NOT
+            </button>
+            <button
+              className={styles.button}
+              onClick={() => addToRule("leftNeighbor")}
+            >
+              Left neighbor
+            </button>
+            <button
+              className={styles.button}
+              onClick={() => addToRule("rightNeighbor")}
+            >
+              Right neighbor
+            </button>
+            <button className={styles.button} onClick={() => addToRule("cell")}>
+              Cell
+            </button>
+            <button
+              className={styles.button}
+              onClick={() => addToRule("=== 1")}
+            >
+              IS FILLED
+            </button>
+            <button className={styles.button} onClick={deleteFromRule}>
+              Delete
+            </button>
+            <button className={styles.button} onClick={clearCustomRule}>
+              Clear
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
